072064b527048f5917e9062aca19718a65b041cd
Peter Graham <peter@wealthsimple.com>
2015-08-12 19:48:05 -0400

    Initial setup script

---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Certain Ruby gems with C extensions require GCC
+xcode-select --install
+
+# Install Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install Redis and start it on boot
+brew install redis
+ln -sfv /usr/local/opt/redis/*.plist ~/Library/LaunchAgents
+launchctl load ~/Library/LaunchAgents/homebrew.mxcl.redis.plist
+launchctl start io.redis.redis-server
+
+# Install rbenv for Ruby version management
+brew install rbenv ruby-build
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
+
+# Install the Ruby versions we use
+rbenv install 2.2.2
+rbenv global 2.2.2
+gem install bundle
+rbenv rehash
+
+# Add postgres binaries to PATH
+echo 'export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/9.4/bin' >>~/.bash_profile
+source ~/.bash_profile
+
+# Create postgres superuser
+createuser -s postgres
+
+# Create databases and hstore extensions
+psql --command="CREATE DATABASE wealthsimple_development;"
+psql --dbname=wealthsimple_development --command="CREATE EXTENSION IF NOT EXISTS hstore;"
+psql --command="CREATE DATABASE wealthsimple_test;"
+psql --dbname=wealthsimple_test --command="CREATE EXTENSION IF NOT EXISTS hstore;"
