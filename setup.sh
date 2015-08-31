783b71f1b661f6ef3c76af84c904756134e4bd3f
Andrew Potapov <andrew@wealthsimple.com>
2015-08-31 17:11:05 -0400

    Replace comments with echo's, move postgres user and database creation to the end (there was an error creating them right after installation of postgres). Added 3 beeps at the end to notify the user when the process is done.

--- setup.sh
+++ setup.sh
@@ -4,44 +4,35 @@ DATABASE_NAMES=("wealthsimple_development" "wealthsimple_test")
 RUBY_VERSIONS=("2.1.5" "2.2.2")
 
 if [[ "$OSTYPE" != "darwin"* ]]; then
-  echo "Error: This script only works on OS X."
+  echo "==========Error: This script only works on OS X.=========="
   exit 1
 fi
 
-# Install & update Homebrew
+echo "==========Install & update Homebrew=========="
 curl -fsSL 'https://raw.githubusercontent.com/Homebrew/install/master/install' | ruby
 brew update
 
-# Allow us to create custom launch agents
+echo "==========Allow us to create custom launch agents=========="
 mkdir -p ~/Library/LaunchAgents
 
-# Install Redis and start it on boot
+echo "==========Install Redis and start it on boot=========="
 brew install redis
 ln -sfv /usr/local/opt/redis/*.plist ~/Library/LaunchAgents
 launchctl load ~/Library/LaunchAgents/homebrew.mxcl.redis.plist
 launchctl start io.redis.redis-server
 
-# Install PostgreSQL and start it on boot
+echo "==========Install PostgreSQL and start it on boot=========="
 brew install postgres
 ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
 launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
 
-# Create postgres superuser
-createuser -s postgres
-
-# Create databases and hstore extensions
-for database_name in "${DATABASE_NAMES[@]}"; do
-  psql --username=postgres --command="CREATE DATABASE ${database_name};"
-  psql --dbname=$database_name --command="CREATE EXTENSION IF NOT EXISTS hstore;"
-done
-
-# Install rbenv for Ruby version management
+echo "==========Install rbenv for Ruby version management=========="
 brew install rbenv ruby-build
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 source ~/.bash_profile
 
-# Install the Ruby versions we use
+echo "==========Install the Ruby versions we use=========="
 for ruby_version in "${RUBY_VERSIONS[@]}"; do
   rbenv install $ruby_version
   rbenv shell $ruby_version
@@ -49,4 +40,18 @@ for ruby_version in "${RUBY_VERSIONS[@]}"; do
 done
 rbenv rehash
 
+echo "==========Create postgres superuser=========="
+createuser -s postgres
+
+echo "==========Create databases and hstore extensions=========="
+for database_name in "${DATABASE_NAMES[@]}"; do
+  psql --username=postgres --command="CREATE DATABASE ${database_name};"
+  psql --dbname=$database_name --command="CREATE EXTENSION IF NOT EXISTS hstore;"
+done
+
+# 3 beep salute
+for i in `seq 3`; do
+    echo -ne '\007'
+    sleep 0.5
+done &
 echo "✨ 💥 Done! 💥 ✨"
